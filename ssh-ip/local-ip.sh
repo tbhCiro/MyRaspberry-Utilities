ae9972d73da5be9710ce4510d8e2cbd26e838129
Ciro <wellalollo@hotmail.it>
2014-12-11 21:47:24 +0100

    Create local-ip.sh

---
+++ ssh-ip/local-ip.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Option: 
+#1. Crond refresh: About 5 min
+
+IPFILE=$HOME/Dropbox/iplocale
+[[ -f $IPFILE ]] || echo 0.0.0.0 > $IPFILE
+
+ACTUALIP=$( networkctl status wlp2s0 | awk '/     Address: / {print $NF}')
+OLDIP=$(cat $IPFILE)
+
+
+if [ "$ACTUALIP" != "$OLDIP" ]; then
+	echo $ACTUALIP > $IPFILE
+fi
+
+#Note: It works if you have Dropboxd process on background and you can see the actual IP on file "iplocale.txt"  
