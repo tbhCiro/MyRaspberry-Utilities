5928e711f49698b2e23207727a77fd59a9f9862b
Ciro <wellalollo@hotmail.it>
2014-12-11 21:58:46 +0100

    Create external-ip.sh

---
+++ ssh-ip/external-ip.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+IPFILE=$HOME/Dropbox/raspberry/ipexternal
+[[ -f $IPFILE ]] || echo 0.0.0.0 > $IPFILE
+
+
+#Option:
+#1. Crond refresh: About 5 min
+
+ACTUALIP=$(curl -s ipinfo.io/ip)
+
+#echo $ACTUALIP > $IPFILE
+
+OLDIP=$(cat $IPFILE)
+
+if [ "$ACTUALIP" != "$OLDIP" ]; then
+	echo $ACTUALIP > $IPFILE
+fi
+
+
+#Note: It works if you have Dropboxd process on background , so you can see the actual IP on file "ipexternal.txt" 
